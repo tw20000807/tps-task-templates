9c634a56f222b8d9288e5ae69ca84ff8304a2369
root <root@DESKTOP-21614UA.>
2026-05-15 13:41:59 +0800

    fix

--- default/scripts/internal/json_extract.py
+++ default/scripts/internal/json_extract.py
@@ -17,6 +17,7 @@ def navigate_json(data, path, json_file_name):
                 raise KeyError
         except (KeyError, IndexError):
             sys.stderr.write("Requested key '%s' not found in '%s'\n" % (path, os.path.basename(json_file_name)))
+            sys.exit(4)
     return data
 
 
